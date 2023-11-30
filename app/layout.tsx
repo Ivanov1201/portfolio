4fcc8c29ea94d522707f1e8f68c5ae10a5ed39cf
Ivanov1201 <vitaliy.ivanov1201@gmail.com>
2023-11-30 10:54:48 +0200

    :bug: fix: build error

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,6 @@
-'use client'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
-
-import { usePageCheck } from '@/app/hooks'
-import Navbar from '@/app/components/navbar'
-import Sidebar from '@/app/components/sidebar'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -20,18 +15,11 @@ export default function RootLayout({
 }: {
   children: React.ReactNode
 }) {
-  const isHomePage: boolean = usePageCheck(['/'])
+  
   return (
     <html lang='en'>
       <body className={inter.className}>
-        <div className={`drawer ${!isHomePage ? 'lg:drawer-open' : ''}`}>
-          <input id='my-drawer' type='checkbox' className='drawer-toggle' />
-          <div className='drawer-content'>
-            <Navbar />
-            {children}
-          </div>
-          <Sidebar />
-        </div>
+        {children}
       </body>
     </html>
   )
