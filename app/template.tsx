b951f095f74fb5a5e7525484520d3506f6d8d9c9
Ivanov1201 <vitaliy.ivanov1201@gmail.com>
2023-11-30 22:12:59 +0200

    :construction: feat: add react-query

--- app/template.tsx
+++ app/template.tsx
@@ -1,7 +1,12 @@
 'use client'
+import React from 'react'
 import Navbar from '@/app/_components/navbar'
 import Sidebar from '@/app/_components/sidebar'
 import { usePageCheck } from '@/app/_hooks/use-page-check'
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
 
 export default function RootTemplate({
   children,
@@ -9,14 +14,19 @@ export default function RootTemplate({
   children: React.ReactNode
 }) {
   const isHomePage: boolean = usePageCheck(['/'])
+  // const [queryClient] = React.useState(() => new QueryClient())
+  const queryClient = new QueryClient()
+
   return (
-    <div className={`drawer ${!isHomePage ? 'lg:drawer-open' : ''}`}>
-      <input id='my-drawer' type='checkbox' className='drawer-toggle' />
-      <div className='drawer-content'>
-        <Navbar />
-        {children}
+    <QueryClientProvider client={queryClient}>
+      <div className={`drawer ${!isHomePage ? 'lg:drawer-open' : ''}`}>
+        <input id='my-drawer' type='checkbox' className='drawer-toggle' />
+        <div className='drawer-content'>
+          <Navbar />
+          {children}
+        </div>
+        <Sidebar />
       </div>
-      <Sidebar />
-    </div>
+    </QueryClientProvider>
   )
 }
