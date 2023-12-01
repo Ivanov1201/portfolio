68cb4505dd4bd2476e7378cd1a63d869377c0a79
Ivanov1201 <vitaliy.ivanov1201@gmail.com>
2023-12-01 23:16:52 +0200

    :label: feat: add type for projects data

--- app/constants.ts
+++ app/constants.ts
@@ -8,3 +8,21 @@ export const routes: Routes = {
   Projects: '/projects',
   Contact: '/contact',
 }
+
+export enum ProjectType {
+  PERSONAL = 'personal', // Personal Side Project
+  CLIENT = 'client', // Client Contract Project
+  CORPORATE = 'corporate', // Corporate/Company Project
+  CONSULTING = 'consulting', // Consulting Project
+  COLLABORATE = 'collaborate', // Collaborative Project
+}
+
+export type Project = {
+  id: number
+  type: ProjectType
+  title: string
+  description: string
+  live_url?: string
+  source_url?:string
+  tags: string[]
+}
